81a2553adec77029f5a127d67912fd5030e2c491
zhangzifan716 <zhangzifan716@gmail.com>
2022-03-15 20:46:11 -0400

    commit

--- docker/build.sh
+++
@@ -1,3 +0,0 @@
-docker build -t ns3-gym-csmarl:base -f Dockerfile_base .
-docker build -t ns3-gym-csmarl:git -f Dockerfile_from_github .
-docker build -t ns3-gym-csmarl:src -f Dockerfile_from_source ..
--- docker/tb.sh
+++
@@ -1,7 +0,0 @@
-if [ $# -lt 1 ]; then
-    echo "usage:" $0 "[container_name]"
-    exit 1
-fi
-
-# run tensorboard inside container
-docker exec -d $1 tensorboard --logdir=~/ray_results --bind_all
